a94ae733364dcafba6f66c720424a6eff30d0037
vasantha <chilukuri.vs@gmail.com>
2022-02-16 09:36:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    let cities = ["Goa", "Darjeeling", "Lonavala"];
+
+    return (
+      <ol>
+        {cities.map((city, index) => (
+          <li key={`location${index + 1}`}>{city}</li>
+        ))}
+      </ol>
+    );
+  }
 }
 
 export default App;
